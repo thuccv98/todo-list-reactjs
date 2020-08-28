39533d880552593aa331aa338c74937a2bd3f352
thuccv98 <thuccv98@gmail.com>
2020-08-28 22:47:09 +0700

    using prettier to format code

--- src/App.js
+++ src/App.js
@@ -8,23 +8,21 @@ class App extends Component {
     this.todoItems = [
       { title: 'Mua bim bim', isComplete: true },
       { title: 'Mua kẹo mút' },
-      { title: 'Đi xem phim' }
+      { title: 'Đi xem phim' },
     ];
   }
 
   render() {
     return (
       <div className="App">
-      {
-        this.todoItems.length > 0 && this.todoItems.map((item, index) => <TodoItem key={index} item={item} />)
-      }
-      {
-        this.todoItems.length === 0 && 'Nothing here'
-      }
-    </div>
+        {this.todoItems.length > 0 &&
+          this.todoItems.map((item, index) => (
+            <TodoItem key={index} item={item} />
+          ))}
+        {this.todoItems.length === 0 && 'Nothing here'}
+      </div>
     );
   }
 }
-
 
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import './TodoItem.css';
 
 class TodoItem extends Component {
-    render() {
-        const { item } = this.props;
-        let className = 'TodoItem';
-        if (item.isComplete) {
-            className += ' TodoItem-complete';
-        }
-        return (
-            <div className={className}>
-                <p> {this.props.item.title} </p>
-            </div>
-        );
+  render() {
+    const { item } = this.props;
+    let className = 'TodoItem';
+    if (item.isComplete) {
+      className += ' TodoItem-complete';
     }
+    return (
+      <div className={className}>
+        <p> {this.props.item.title} </p>
+      </div>
+    );
+  }
 }
 
 export default TodoItem;
