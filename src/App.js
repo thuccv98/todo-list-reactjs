0a4169db2ecb660633fd3d7059d9b99c9f9f5ffb
thuccv98 <thuccv98@gmail.com>
2020-08-28 21:26:40 +0700

    commit tu dau cho den bai 11

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import TodoItem from './components/TodoItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor() {
+    super();
+    this.todoItems = [
+      { title: 'Mua bim bim', isComplete: true },
+      { title: 'Mua kẹo mút' },
+      { title: 'Đi xem phim' }
+    ];
+  }
+
+  render() {
+    return (
+      <div className="App">
+      {
+        this.todoItems.length > 0 && this.todoItems.map((item, index) => <TodoItem key={index} item={item} />)
+      }
+      {
+        this.todoItems.length === 0 && 'Nothing here'
+      }
     </div>
-  );
+    );
+  }
 }
+
 
 export default App;
